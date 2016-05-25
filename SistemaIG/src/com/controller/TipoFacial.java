c1440841fae76a04427d64f4d16b6c2b8e135d71
janioxavier <janioxavier@A204-09.imd.local>
2016-05-25 16:53:59 -0300

    "Enumeração dos tipos de ações corporais e faciais e dos personagens do jogo"

---
+++ SistemaIG/src/com/controller/TipoFacial.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+/**
+ *
+ * @author janioxavier
+ */
+public enum TipoFacial implements Acao{
+    PISCAR_1_OLHO,
+    PISCAR_2_OLHOS,
+    ESTIRAR_LINGUA,
+    ABRIR_BOCA,
+    FAZER_CARA_CHORO,
+    FAZER_CARETA,
+    SORRIR;
+
+    @Override
+    public Acao getTipoAcao() {
+        return this;
+    }
+}
---
+++ SistemaIG/src/com/controller/TipoPersonagem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+/**
+ *
+ * @author janioxavier
+ */
+public enum TipoPersonagem {
+    GATO,
+    CACHORRO,
+    MENINO,
+    MENINA;
+}
